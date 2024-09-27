9a2fe6162e82460f6ebd25db595d37484eab728c
subash-RedXnzero <subashchy245@gmail.com>
2024-09-27 14:38:59 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
+
+
+
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.hero'),
+    smooth: true
+});
